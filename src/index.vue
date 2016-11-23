faa144f0ab7b4bd8d97bae4c94b6e9c0cf4f1b4b
liujians <870891018@qq.com>
2016-11-23 16:44:03 +0800

    更新、移动端菜单跳转自动收回菜单

--- src/index.vue
+++ src/index.vue
@@ -417,3 +417,33 @@
     <div class="control-sidebar-bg"></div>
   </div>
 </template>
+<script>
+  export default {
+  data() {
+    return {
+        
+    }
+  },
+  components: {
+    
+  },
+  mounted(){
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+      alert(1);
+        $("a[href!='#']").attr("data-toggle","offcanvas");
+    }else{
+      // alert(2);
+    }
+  },
+  methods: {}
+}
+</script>
